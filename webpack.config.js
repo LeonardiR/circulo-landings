82afdea756b3980ae0e45a8af62793b81b7cd897
Regulo Leonardi <regulo.leonardipirela@altran.com>
2019-02-06 17:14:54 +0100

    First circulo landing

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
                     {
                         loader: "css-loader",
                         options: {
-                            sourceMap: isDevelopment,
+                            sourceMap: isDevelopment
                         }
                     },
                     {
